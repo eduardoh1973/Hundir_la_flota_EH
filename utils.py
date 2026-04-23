285a3dc33a9b8a1ddf9149d18a76f54a62a69864
eduardoh1973 <eduardoh.servibat@gmail.com>
2026-04-23 10:15:29 +0200

    s

---
+++ utils.py
@@ -0,0 +1,57 @@
+import numpy as np
+def crear_tablero():
+    tablero = np.full((10,10), " ")
+    return tablero
+
+def pedir_barcos_jugador():
+    pos_inicial_f = int(input("fila"))
+    pos_inicial_c = int(input("columna"))
+    inicio = (pos_inicial_f, pos_inicial_c)
+
+def colocar_barcos(lista_barcos, tablero):
+    for i in lista_barcos:
+        for j in i:
+            tablero[j] ="o"
+    return tablero
+
+def disparar(tablero):
+    fila = int(input ("fila: "))
+    columna = int(input ("columna: "))
+
+    if tablero[fila,columna] == "o":
+        tablero[fila,columna] = "X"
+        print("tocado")
+        return True
+    else:
+        tablero[fila,columna] = "A"
+        print("agua")
+        return False
+     
+    
+    
+def crear_barco_eslora(eslora):
+    tamaño = 10
+    orientacion = random.choice(["H", "V"])
+
+    if orientacion == "H":
+        fila = random.randint(0, tamaño - 1)
+        col_inicio = random.randint(0, tamaño - eslora)
+        return [(fila, col_inicio + i) for i in range(eslora)]
+
+    else:
+        fila_inicio = random.randint(0, tamaño - eslora)
+        col = random.randint(0, tamaño - 1)
+        return [(fila_inicio + i, col) for i in range(eslora)]
+
+
+
+def crear_lista_barcos():
+    lista_esloras = (2,2,2,3,3,4)
+    lista_barcos =[]
+
+    for i in lista_esloras:
+        barco = crear_barco(i)
+        lista_barcos.append(barco)
+
+#añadir que si se pisan, vuelve a crear, hasta que no se pisen
+
